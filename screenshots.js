f8a012545a43dbc718cf9f636eaec16b7271a014
Ingmar Drewing <idrewing@Ingmars-MacBook-Pro.local>
2013-01-18 14:27:04 +0100

    Index angepasst, weitere Screenshots

--- screenshots.js
+++ screenshots.js
@@ -3,7 +3,7 @@ Script for taking screenshots.
 Run with "casperjs screenshots.js"
 */
 
-var index_path = '/Users/drewing/Sites/hackathonStyleguide/mockup/index.html';
+var index_path = '/Users/idrewing/Sites/hackathonStyleguide/mockup/index.html';
 
 var casper = require('casper').create({
 	viewportSize: {
@@ -20,11 +20,35 @@ casper.then(function () {
 });
 
 casper.then(function () {
-	this.captureSelector('title.png', '#title-heading');
+	this.captureSelector('title.png', '.screenshot_heading_pageinfo');
 });
 
 casper.then(function () {
-	this.captureSelector('table.png', '.table-wrap');
+	this.captureSelector('toc.png', '.screenshot_toc');
+});
+
+casper.then(function () {
+	this.captureSelector('headings.png', '.screenshot_headings');
+});
+
+casper.then(function () {
+	this.captureSelector('list.png', '.screenshot_list');
+});
+
+casper.then(function () {
+	this.captureSelector('numberedlist.png', '.screenshot_numberedlist');
+});
+
+casper.then(function () {
+	this.captureSelector('panel.png', '.screenshot_panel');
+});
+
+casper.then(function () {
+	this.captureSelector('hr.png', '.screenshot_hr');
+});
+
+casper.then(function () {
+	this.captureSelector('table.png', '.screenshot_tables');
 });
 
 casper.run();
