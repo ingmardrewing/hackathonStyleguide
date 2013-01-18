72fb91270f2389d5791ae23da04c64867760e7d1
Ingmar Drewing <ingmar@drewing.de>
2013-01-18 13:35:43 +0100

    fixed error

---
+++ artifacts/dec1f966613294b7c56c89bb3c3b4c3d.js
@@ -0,0 +1,30 @@
+/*
+Script for taking screenshots.
+Run with "casperjs screenshots.js"
+*/
+
+var index_path = '/Users/drewing/Sites/hackathonStyleguide/mockup/index.html';
+
+var casper = require('casper').create({
+	viewportSize: {
+		width: 800,
+		height: 600
+	}
+});
+
+casper.start(index_path, function() {
+});
+
+casper.then(function () {
+	this.captureSelector('tasklist.png', '.inline-task-list');
+});
+
+casper.then(function () {
+	this.captureSelector('title.png', '#title-heading');
+});
+
+casper.then(function () {
+	this.captureSelector('table.png', '.table-wrap');
+});
+
+casper.run();
