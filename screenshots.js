f50545688fc015f99aa07abfb075a505ee82f7eb
Ingmar Drewing <idrewing@Ingmars-MacBook-Pro.local>
2013-01-18 12:20:09 +0100

    styleguide erweitert, partials ergänzt

--- screenshots.js
+++ screenshots.js
@@ -3,7 +3,7 @@ Script for taking screenshots.
 Run with "casperjs screenshots.js"
 */
 
-var index_path = '/Users/kbecker/hackathon/hackathonStyleguide/mockup/index.html'
+var index_path = '/Users/idrewing/Sites/hackathonStyleguide/mockup/index.html'
 
 var casper = require('casper').create({
 	viewportSize: {
@@ -18,5 +18,13 @@ casper.start(index_path, function() {
 casper.then(function () {
 	this.captureSelector('tasklist.png', '.inline-task-list');
 });
-	
+
+casper.then(function () {
+	this.captureSelector('title.png', '#title-heading');
+});
+
+casper.then(function () {
+	this.captureSelector('table.png', '.table-wrap');
+});
+
 casper.run();
